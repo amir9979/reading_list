022fa33c3848d8f71edb923ace119dc9a46dc78c
Amir Elmishali <amir9979@gmail.com>
2024-03-13 12:08:10 +0200

    Update process_issue.py

--- .github/scripts/process_issue.py
+++ .github/scripts/process_issue.py
@@ -10,7 +10,7 @@ def parse_email_content(email_content):
     link_start_sep = 'https://scholar.google.co.uk/scholar_url?url='
     link_end_sep = '&hl=en'
 
-    print(str(email_content[email_content.index('__\n')-10:email_content.index('__\n')-10+50]))
+    print(str(email_content[email_content.index('__\n')-10:email_content.index('__\n')-10+80]))
     email_content.split(start)[1]
     email_content.split(start)[1].split(sep)
     papers = [x.split(save_sep)[0] for x in email_content.split(start)[1].split(sep)][:-1]
@@ -35,4 +35,4 @@ def parse_email_content(email_content):
 
     return relevant_info
 
-print(parse_email_content(re.sub(' +', ' ',os.environ['ISSUE_BODY'])))
+print(parse_email_content(os.environ['ISSUE_BODY']))
