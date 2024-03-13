2997d69ae90369786e90d0684b3829caef2041ef
Amir Elmishali <amir9979@gmail.com>
2024-03-13 11:49:16 +0200

    Update process_issue.py

--- .github/scripts/process_issue.py
+++ .github/scripts/process_issue.py
@@ -10,6 +10,7 @@ def parse_email_content(email_content):
     link_start_sep = 'https://scholar.google.co.uk/scholar_url?url='
     link_end_sep = '&hl=en'
 
+    print(len(email_content))
     papers = [x.split(save_sep)[0] for x in email_content.split(start)[1].split(sep)][:-1]
     papers = [p.split(initial_sep)[1] for p in papers]
     relevant_info = []
