3a919fc4aee08f22bcf6e66ecadaca5836319304
Amir Elmishali <amir9979@gmail.com>
2025-05-20 23:02:51 +0300

    Update process_issue.py

--- .github/scripts/process_issue.py
+++ .github/scripts/process_issue.py
@@ -29,4 +29,4 @@ def parse_email_content(email_content):
 
     return json.dumps(relevant_info)
 
-# print(parse_email_content(os.environ['ISSUE_BODY']))
+print(parse_email_content(os.environ['ISSUE_BODY']))
